7679f339610fdfab2574c65b8b0e88c96f4df263
CodeWithKrushnal <krushnal.patil@joshsoftware.com>
2025-02-07 19:33:39 +0530

    Add Assignment 3 # Add Assignment 3

---
+++ Assignment3/Q2-Youtube-Playback-Speed.js
@@ -0,0 +1,9 @@
+function playbackChanger() {
+    document.querySelector("video").playbackRate = 10
+    return
+}
+
+playbackChanger()
+
+// Paste This Code into Browser console while playing the content.
+// Results in changing playback speed to 10x.
